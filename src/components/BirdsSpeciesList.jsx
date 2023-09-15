67f2ab98afa156d76a1620f92104be65d22ebcb9
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-09-15 01:25:21 +0100

    add banner to the home page

--- src/components/BirdsSpeciesList.jsx
+++ src/components/BirdsSpeciesList.jsx
@@ -36,15 +36,25 @@ const BirdsSpeciesList = () => {
   };
 
   return (
-    <ul className="home-container">
-      {birdsStore.birds.map((speccy) => (
-        <li key={speccy.speciesCode} className={darkOrLight() ?? ''}>
-          <Link to={`/details/${speccy.speciesCode}`}>
-            <BirdSpeccy speccy={speccy} />
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <div className="banner">
+        <div className="year">2023</div>
+        <h2 className="banner-title">Birds Speccies</h2>
+        <div>
+          <span>Total:</span>
+          <span>{birdsStore.birds?.length}</span>
+        </div>
+      </div>
+      <ul className="home-container">
+        {birdsStore.birds.map((speccy) => (
+          <li key={speccy.speciesCode} className={darkOrLight() ?? ''}>
+            <Link to={`/details/${speccy.speciesCode}`}>
+              <BirdSpeccy speccy={speccy} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
